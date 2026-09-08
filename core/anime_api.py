919bd97c13eff9dfcaf14b3bc4ebaeb7c3964506
×͜× 「 ʟᴏꜱᴛ ᴇxɪꜱᴛᴇɴᴄᴇ 」 ×͜× <thebochan9@gmail.com>
2026-09-08 19:18:26 +0530

    Update anime_api.py

--- core/anime_api.py
+++ core/anime_api.py
@@ -2,20 +2,19 @@ import aiohttp
 import requests
 from bs4 import BeautifulSoup
 
-class AnimePaheAPI:
-    def __init__(self):
-        self.base_url = "https://animepahe.pw"
+base_url = "https://animepahe.pw"
 
-    def search_anime(self, query):
-        url = f"{self.base_url}/api?m=search&q={query}"
-        response = requests.get(url)
-        if response.status_code == 200:
-            return response.json().get("data", [])
-        return []
+def search_anime(query):
+    url = f"{base_url}/api?m=search&q={query}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json().get("data", [])
+    return []
 
-    def get_episodes(self, anime_session):
-        url = f"{self.base_url}/api?m=release&id={anime_session}&sort=episode_asc&page=1"
-        response = requests.get(url)
-        if response.status_code == 200:
-            return response.json().get("data", [])
-        return []
+def get_episodes(anime_session):
+    url = f"{base_url}/api?m=release&id={anime_session}&sort=episode_asc&page=1"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json().get("data", [])
+    return []
+
